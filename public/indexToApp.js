81c13effedc74743512283b1f2c0f99b6eb17a02
Louis Parkes <67322709+LouisP03@users.noreply.github.com>
2021-04-27 20:58:22 +0100

    validation

--- public/indexToApp.js
+++ public/indexToApp.js
@@ -4,6 +4,11 @@ function getForm() {
     var username = String(document.getElementById('usernameEntry').value);
     var radio = String(document.querySelector('input[name="room"]:checked').value);
 
+    //if username length is greater than 12...
+    if (username.length > 12) {
+        //...break out of function/do nothing
+        return false;
+    }
     //form the query string 
     var queryString = `?roomID=${radio}&username=${username}`;
     var URL = 'https://compsci-project-2021.herokuapp.com/app.html'+queryString;
